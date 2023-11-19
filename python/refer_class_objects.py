17ff09f7c86b4328fc3095322651dcd7116d6eb7
Jianjun Dai <95723518+jianjundai2021@users.noreply.github.com>
2023-11-20 12:35:42 +1300

    python learning refer self method object

---
+++ python/refer_class_objects.py
@@ -0,0 +1,8 @@
+# class A 
+# class A import logging
+# defines logger = logging.getLogger(__name__)
+
+# class B inherits from A
+# class B to use logger 
+# just need to say self.logger.info("message")
+
